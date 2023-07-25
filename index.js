da0bbb69b365a30b03e673f5336bccb7dec44218
SkyKeyUA <veremiienkoov96@gmail.com>
2023-07-25 17:07:50 +0300

    express-fileupload was installed

--- index.js
+++ index.js
@@ -7,11 +7,13 @@ import DataBase from './db.js';
 import models from './models/models.js';
 import { initRoutes } from './routes/index.js';
 import { errorMiddleware } from './middlewares/errorMiddleware.js';
+import fileUpload from 'express-fileupload';
 
 const PORT = process.env.PORT || 7000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
 
 initRoutes(app);
 app.use(errorMiddleware);
