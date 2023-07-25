881401fb84588b0588037087b2220438902e01f7
SkyKeyUA <veremiienkoov96@gmail.com>
2023-07-25 20:23:41 +0300

    devicecontroller (create) was created

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,9 +1,33 @@
 /** @format */
 
-class DeviceController {
-  async create(req, res) {}
+import { v4 as uuidv4 } from 'uuid';
+import { Device } from '../models/models.js';
+import { ApiError } from '../exceptions/ApiError.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-  async getAll(req, res) {}
+class DeviceController {
+  async create(req, res, next) {
+    try {
+      const __filename = fileURLToPath(import.meta.url);
+      const __dirname = path.dirname(__filename);
+      const { name, price, brandId, typeId, info } = req.body;
+      const { img } = req.files;
+      let fileName = uuidv4() + '.jpg';
+      img.mv(path.resolve(__dirname, '..', 'static', fileName));
+
+      const device = await Device.create({ name, price, brandId, typeId, img: fileName });
+
+      return res.json(device);
+    } catch (e) {
+      next(ApiError.badRequest(e.massage));
+    }
+  }
+
+  async getAll(req, res) {
+    const types = await Device.findAll();
+    return res.json(types);
+  }
 
   async getOne(req, res) {}
 }
--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT || 7000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('static'));
 app.use(fileUpload({}));
 
 initRoutes(app);
